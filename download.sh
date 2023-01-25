902b10b826c03627bbdd49ec38402b91a7a5a21e
emirdeliz <emir.marques@condoconta.com>
2023-01-25 13:56:29 -0300

    feat: Added the github actions

--- download.sh
+++ download.sh
@@ -39,10 +39,10 @@ unzipFiles() {
 	FILENAME_ZIP="$CHROMEDRIVER_FILENAME"
 	CURRENT_DIR=$(pwd)
 
-	echo "$CURRENT_DIR \ $CHROMEDRIVER_FILENAME"
+	echo "$CURRENT_DIR/$CHROMEDRIVER_FILENAME"
 
 	if [ "$OSTYPE" == "msys" ]; then
-		ls && powershell -command "Expand-Archive $CURRENT_DIR\$CHROMEDRIVER_FILENAME CURRENT_DIR" && ls
+		ls && powershell -command "Expand-Archive $CURRENT_DIR/$CHROMEDRIVER_FILENAME $CURRENT_DIR" && ls
 	else
 		FILENAME_ZIP="${CHROMEDRIVER_FILENAME}.zip"
 		unzip "$FILENAME_ZIP"
