f0dce49c72250608177e27c7689d4f6807fccbd2
emirdeliz <emir.marques@condoconta.com>
2023-01-25 18:33:29 -0300

    feat: Updated the github actions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import path from 'path';
+import fs from 'fs';
 import { exec as _exec } from '@actions/exec';
 import { getInput } from '@actions/core';
 
-const version = getInput('version', { required: false });
-const arch = getInput('arch', { required: false });
-const downloadPath = getInput('download-path', { required: false });
+async function run() {
+	const version = getInput('version', { required: false });
+	const arch = getInput('arch', { required: false });
+	const downloadPath = getInput('download-path', { required: false });
 
-await _exec(path.join(__dirname, 'download.sh'), [version, arch, downloadPath]);
+	const shPath = `${fs.realpathSync('.')}/dist/download.sh`;
+	await _exec('sh', shPath, [version, arch, downloadPath]);
+}
+
+run();
