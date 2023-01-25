273cbd6008e8226bee7da679855b2bfe45fd978e
emirdeliz <emir.marques@condoconta.com>
2023-01-25 15:36:39 -0300

    feat: Added the github actions

--- download.sh
+++ download.sh
@@ -24,14 +24,14 @@ getDriverVersion() {
 downloadDriver() {
 	CHROMEDRIVER_RELEASE="$1"
 	CHROMEDRIVER_FILENAME="$2"
-	FILENAME_ZIP="$CHROMEDRIVER_FILENAME"
+	# FILENAME_ZIP="$CHROMEDRIVER_FILENAME"
 
-	if [ "$OSTYPE" != "msys" ]; then
-		FILENAME_ZIP="${CHROMEDRIVER_FILENAME}.zip"
-	fi
+	# # if [ "$OSTYPE" != "msys" ]; then
+	# FILENAME_ZIP="${CHROMEDRIVER_FILENAME}.zip"
+	# # fi
 
 	DONWLOAD_URL="https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_RELEASE/$CHROMEDRIVER_FILENAME"
-	curl --show-error --retry 10 --output "$FILENAME_ZIP" "$DONWLOAD_URL"
+	curl --show-error --retry 10 --output "$CHROMEDRIVER_FILENAME" "$DONWLOAD_URL"
 }
 
 unzipFiles() {
