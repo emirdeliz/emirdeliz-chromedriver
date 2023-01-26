d6afd084ed79bc9b82218d2e25f2fb7d92f53b3e
Emir Marques <emirdeliz@gmail.com>
2023-01-26 14:58:42 -0300

    feat: Work in progress

---
+++ dist/README.md
@@ -0,0 +1,30 @@
+# emirdeliz-chromedriver
+
+[![Linux 64](https://github.com/emirdeliz/emirdeliz-chromedriver/actions/workflows/linux64.yml/badge.svg)](https://github.com/emirdeliz/emirdeliz-chromedriver/actions/workflows/linux64.yml)
+[![Mac 64](https://github.com/emirdeliz/emirdeliz-chromedriver/actions/workflows/mac64.yml/badge.svg)](https://github.com/emirdeliz/emirdeliz-chromedriver/actions/workflows/mac64.yml)
+
+This action sets up a [ChromeDriver](https://chromedriver.chromium.org/) for use in actions.
+
+## OS/Platform support
+
+- ubuntu-latest, ubuntu-20.04 or ubuntu-18.04
+- macos-latest, macos-10.15
+
+# Usage
+
+See [action.yml](action.yml)
+
+# Inputs
+
+```yml
+input:
+  version:
+    description: 'The chromedriver version'
+    default: 'latest'
+  arch:
+    description: 'The chromedriver arch (linux64, mac64 or mac_arm64)'
+    default: 'linux64'
+  download-path:
+    description: 'The path to download chromedriver'
+    default: ''
+```
---
+++ dist/download.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+setDownloadDir() {
+	DOWNLOAD_PATH="$1"
+	if [ "$DOWNLOAD_PATH" != "" ]; then
+		mkdir -p "$DOWNLOAD_PATH" && cd "$DOWNLOAD_PATH" || exit
+	fi
+}
+
+removeObsoleteFiles() {
+	rm -rf chromedriver LICENSE.chromedriver
+}
+
+getDriverVersion() {
+	VERSION="$1"
+	CHROMEDRIVER_RELEASE="$VERSION"
+	if [ "$VERSION" == "latest" ]; then
+		VERSION="LATEST_RELEASE"
+		CHROMEDRIVER_RELEASE="$(curl --show-error --retry 10 "http://chromedriver.storage.googleapis.com/$VERSION")"
+	fi
+	echo "$CHROMEDRIVER_RELEASE"
+}
+
+downloadDriver() {
+	CHROMEDRIVER_RELEASE="$1"
+	CHROMEDRIVER_FILENAME="$2"
+	FILENAME_ZIP="${CHROMEDRIVER_FILENAME}.zip"
+	DONWLOAD_URL="https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_RELEASE/$FILENAME_ZIP"
+	curl -L -o "$FILENAME_ZIP" "$DONWLOAD_URL"
+}
+
+unzipFiles() {
+	CHROMEDRIVER_FILENAME="$1"
+	ls && unzip "${CHROMEDRIVER_FILENAME}.zip" && ls
+}
+
+installDependencies() {
+	apt update && sudo apt install -y libnss3
+}
+
+main() {
+	CHROMEDRIVER_FILENAME="chromedriver_$2"
+	CHROMEDRIVER_RELEASE="$(getDriverVersion "$1")"
+	DOWNLOAD_PATH="$3"
+
+	setDownloadDir "$DOWNLOAD_PATH" &&
+		removeObsoleteFiles &&
+		downloadDriver "$CHROMEDRIVER_RELEASE" "$CHROMEDRIVER_FILENAME" &&
+		unzipFiles "$CHROMEDRIVER_FILENAME"
+		
+
+		if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+			installDependencies
+		fi
+		./chromedriver --version
+}
+
+main "$1" "$2" "$3"
